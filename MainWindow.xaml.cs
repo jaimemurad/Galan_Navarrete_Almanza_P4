5deb398c16271765d118b5e0bb6e3aead995edb0
jaimemurad <jaimemurad@gmail.com>
2025-11-21 13:04:22 -0600

    Práctica 4
    Estos son nuestros archivos para la Práctica 4 ;)))

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestorProductosWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GestorProductos gestor = new GestorProductos();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CargarDatosIniciales();
+
+            dataGridProductos.ItemsSource = gestor.ObtenerListaProductos();
+
+            comboTipoBusqueda.Items.Add("ID");
+            comboTipoBusqueda.Items.Add("Nombre");
+            comboTipoBusqueda.SelectedIndex = 0;
+
+            comboCriterioOrden.Items.Add("ID");
+            comboCriterioOrden.Items.Add("Precio");
+            comboCriterioOrden.Items.Add("Nombre");
+            comboCriterioOrden.SelectedIndex = 0;
+
+        }
+
+        private void CargarDatosIniciales()
+        {
+            gestor.AgregarProducto(new Producto
+            {
+                Id = 3,
+                codigoBarras = "789456",
+                Nombre = "Teclado",   
+                Categoria = "Electronica",
+                Precio = 300,
+                Stock = 200,
+            }
+            );
+            gestor.AgregarProducto(new Producto
+            {
+                Id = 15,
+                codigoBarras = "123456",
+                Nombre = "Computadora",
+                Categoria = "Electronica",
+                Precio = 12000,
+                Stock = 10,
+            }
+            );
+            gestor.AgregarProducto(new Producto
+            {
+                Id = 1,
+                codigoBarras = "147852",
+                Nombre = "Bocina",
+                Categoria = "Electronica",
+                Precio = 4000,
+                Stock = 2,
+            }
+            );
+            gestor.AgregarProducto(new Producto
+            {
+                Id = 5,
+                codigoBarras = "258963",
+                Nombre = "USB",
+                Categoria = "Electronica",
+                Precio = 150,
+                Stock = 30,
+            }
+            );
+            gestor.AgregarProducto(new Producto
+            {
+                Id = 9,
+                codigoBarras = "369258",
+                Nombre = "Sudadera",
+                Categoria = "Ropa",
+                Precio = 600,
+                Stock = 15,
+            }
+            );
+        }
+
+        private void MostrarResultado(Producto producto,int iteraciones)
+        { 
+            txtResultadoBusqueda.Text = producto?.ToString() ?? "Producto no encontrado";
+            progressIteraciones.Value = iteraciones*10;
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            var ventanaAgregar = new AgregarProductoWindow();
+            if (ventanaAgregar.ShowDialog() == true)
+            {
+                Producto nuevoProducto = ventanaAgregar.Producto; //Asigna el producto creado en la ventana
+                try
+                {
+                    gestor.AgregarProducto(nuevoProducto);
+                    dataGridProductos.ItemsSource = null;
+                    dataGridProductos.ItemsSource = gestor.ObtenerListaProductos();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void btnOrdenar_Click(object sender, RoutedEventArgs e)
+        {
+            List<Producto> productos = new List<Producto>(gestor.ObtenerListaProductos());
+            string criterio = comboCriterioOrden.SelectedItem.ToString();
+
+            switch (criterio)
+            {
+                case "ID":
+                    OrdenadorSimplificado.QuickSortPorId(productos);
+                    break;
+                case "Precio":
+                    OrdenadorSimplificado.QuickSortPorPrecio(productos);
+                    break;
+                case "Nombre":
+                    productos = OrdenadorSimplificado.MergeSortPorNombre(productos);
+                    break;
+            }
+            listViewOrdenados.ItemsSource = productos;
+            DibujarGraficoBarras(productos);
+        }
+
+        private void DibujarGraficoBarras(List<Producto> productos)
+        {
+            canvasGrafico.Children.Clear();
+            double maxPrecio = (double)productos.Max(p => p.Precio);
+            double escala = canvasGrafico.ActualHeight / maxPrecio;
+
+            for (int i=0; i<productos.Count; i++)
+            { 
+                Rectangle barra = new Rectangle
+                {
+                    Width = 30,
+                    Height = (double)productos[i].Precio * escala,
+                    Fill = Brushes.Purple,
+                    Margin = new Thickness(i * 60, canvasGrafico.ActualHeight - ((double)productos[i].Precio * escala), 0, 0)
+                };  
+
+                canvasGrafico.Children.Add(barra);
+                TextBlock etiqueta = new TextBlock
+                {
+
+                    Text = productos[i].Nombre,
+                    Width = 50, // Limitar el ancho para evitar desbordamiento
+                    TextWrapping = TextWrapping.Wrap, // Permitir que el texto se ajuste automáticamente
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Margin = new Thickness(i * 60, canvasGrafico.ActualHeight - 20, 0, 0) // Ajustar posición vertical
+
+                };  
+                canvasGrafico.Children.Add(etiqueta);
+
+            }
+
+        }
+
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if(dataGridProductos.SelectedItem is Producto productoSeleccionado)
+            {
+                bool eliminado = gestor.EliminarProducto(productoSeleccionado.codigoBarras);
+                if(eliminado)
+                {
+                    dataGridProductos.ItemsSource = null;
+                    dataGridProductos.ItemsSource = gestor.ObtenerListaProductos();
+
+                    MessageBox.Show("Producto eliminado correctamente.", "Eliminado", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Selecciona un producto a eliminar", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void btnBuscar_Click_1(object sender, RoutedEventArgs e)
+        {
+            string criterio = comboTipoBusqueda.SelectedItem.ToString();
+            string valor = txtBusqueda.Text;
+
+            //La lista ordenada
+            List<Producto> productosParaBusqueda = new List<Producto>(gestor.ObtenerListaProductos());
+            OrdenadorSimplificado.QuickSortPorId(productosParaBusqueda); //Ordenar por Id para busqueda binaria
+
+            switch (criterio)
+            {
+                case "ID":
+                    if (int.TryParse(valor, out int id))
+                    {
+                        var (producto, iteraciones) = BuscadorSimplificado.BusquedaBinaria(productosParaBusqueda, id);
+                        MostrarResultado(producto, iteraciones);
+                    }
+                    break;
+                case "Nombre":
+                    var (productoNombre, iteracionesNombre) = BuscadorSimplificado.BusquedaSecuencialNombre(productosParaBusqueda, valor);
+                    MostrarResultado(productoNombre, iteracionesNombre);
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Practica3.cs
@@ -0,0 +1,309 @@
+﻿
+//Actividad 1
+
+public class Producto
+    {
+    public string Nombre { get; set; }
+    public int Id { get; set; }
+
+    public string codigoBarras { get; set; } //Campo unico
+
+   public string Categoria { get; set; }
+
+    public decimal Precio { get; set; }
+
+    public int Stock { get; set; }
+
+    public override string ToString()
+    {
+        return $"[{Id}] {codigoBarras} - {Nombre} | {Precio:C} | Stock: {Stock} |{Categoria}";
+
+    }
+}
+
+public class GestorProductos
+{
+    //LISTA: para mantener orden de insercion
+    private List<Producto> listaProductos = new List<Producto>();
+
+    //DICCIONARIO: para busquedas rapidas por codigo de barras
+    private Dictionary<string, Producto> diccionarioPorCodigo = new Dictionary<string, Producto>();
+
+    public List<Producto> ObtenerListaProductos()
+    {
+        return new List<Producto>(listaProductos);
+    }
+
+    //Operaciones LISTA
+    public void AgregarProducto(Producto p)
+    {
+        //Validar codigo de barras unico
+        if (diccionarioPorCodigo.ContainsKey(p.codigoBarras))
+        {
+            throw new Exception("El producto con este codigo de barras ya existe.");
+
+        }
+        listaProductos.Add(p);
+        diccionarioPorCodigo[p.codigoBarras] = p;
+
+    }
+
+    public void MostrarProductosPorCategoria(string categoria)
+    {
+        //Usamos lista para recorridos secuenciales
+        Console.WriteLine($"Productos en la categoria: {categoria}");
+        foreach (var item in listaProductos)
+        {
+            if (item.Categoria.Equals(categoria, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(item.ToString);
+            }
+        }
+
+    }
+
+
+    public void MostrarInventario()
+    {
+        Console.WriteLine("Inventario en orden de ingreso:");
+        foreach (var producto in listaProductos)
+        {
+            Console.WriteLine(producto);
+        }
+    }
+
+    //Operaciones con DICCIONARIO (busquedas especificas)
+    public Producto BuscarPorCodigo(string codigoBarras)
+    {
+        return diccionarioPorCodigo.TryGetValue(codigoBarras, out var producto) ? producto : null;
+
+    }
+
+    public bool ExisteProducto(string codigoBarras)
+    {
+        return diccionarioPorCodigo.ContainsKey(codigoBarras);
+    }
+
+
+    public bool EliminarProducto(string codigoBarras)
+    {
+        if (diccionarioPorCodigo.TryGetValue(codigoBarras, out var producto))
+        {
+            listaProductos.Remove(producto);
+            diccionarioPorCodigo.Remove(codigoBarras);
+            return true;
+        }
+        return false;
+    }
+}
+    //Actividad 2 Ordenamiento
+
+public class OrdenadorSimplificado
+{
+        //Ordenamiento usando QuickSort
+        public static void QuickSortPorId(List<Producto> productos)
+        {
+            if(productos.Count <= 1)
+            
+                return;
+
+            // 1- Seleccionar un elemento como pivote (aqui se elige el ultimo elemento de la lista)
+            Producto pivote = productos[productos.Count - 1];
+
+            //2- Reorganizar lista con elementos menores a la izquierda y mayores a la derecha
+            var menores = new List<Producto>();
+            var mayores = new List<Producto>();
+
+            for (int i = 0; i < productos.Count - 1; i++)
+            {
+                if (productos[i].Id < pivote.Id)
+                {
+                    menores.Add(productos[i]);
+                }
+                else
+                {
+                    mayores.Add(productos[i]);
+                }
+            }
+
+            // 3- Recursivamente, aplica el algoritmo a las sublistas formadas
+            QuickSortPorId(menores);
+            QuickSortPorId(mayores);
+
+            // 4- Combinar las sublistas para obtener la lista ordenada
+            productos.Clear();
+            productos.AddRange(menores);
+            productos.Add(pivote);
+            productos.AddRange(mayores);
+
+            
+
+          
+        }
+
+	public static void QuickSortPorPrecio(List<Producto> productos)
+	{
+		if (productos.Count <= 1)
+
+			return;
+
+		// 1- Seleccionar un elemento como pivote (aqui se elige el ultimo elemento de la lista)
+		Producto pivote = productos[productos.Count - 1];
+
+		//2- Reorganizar lista con elementos menores a la izquierda y mayores a la derecha
+		var menores = new List<Producto>();
+		var mayores = new List<Producto>();
+
+		for (int i = 0; i < productos.Count - 1; i++)
+		{
+			if (productos[i].Precio < pivote.Precio)
+			{
+				menores.Add(productos[i]);
+			}
+			else
+			{
+				mayores.Add(productos[i]);
+			}
+		}
+
+		// 3- Recursivamente, aplica el algoritmo a las sublistas formadas
+		QuickSortPorPrecio(menores);
+		QuickSortPorPrecio(mayores);
+
+		// 4- Combinar las sublistas para obtener la lista ordenada
+		productos.Clear();
+		productos.AddRange(menores);
+		productos.Add(pivote);
+		productos.AddRange(mayores);
+
+
+
+
+	}
+
+
+
+	//Ordenamiento por MergeSort
+
+	public static List<Producto> MergeSortPorNombre(List<Producto> productos)
+    {
+        if (productos.Count <= 1)
+            return productos;
+
+
+       //1- Dividir lista en dis partes (mitad)
+       int mitad = productos.Count / 2;
+       var izquierda = productos.GetRange(0, mitad);
+       var derecha = productos.GetRange(mitad, productos.Count - mitad);
+
+        //2- Repite proceso para cada mitad (hasta tener solo un elemento)
+        izquierda = MergeSortPorNombre(izquierda);
+        derecha = MergeSortPorNombre(derecha);
+
+        //3- Mezclar las dos mitades ordenadas
+        return Mezclar(izquierda, derecha);
+
+    }
+
+    private static List<Producto> Mezclar(List<Producto> izquierda, List<Producto> derecha)
+    {
+
+        //Comparar el primer elemento de cada mitad 
+        // * El menor de coloca primero en la nueva lista
+        var resultado = new List<Producto>();
+        int i = 0, j = 0;
+
+        //Comparamos y agregamos orden 
+        while (i < izquierda.Count && j < derecha.Count)
+        {
+            if (string.Compare(izquierda[i].Nombre, derecha[j].Nombre) < 0)
+            {
+                resultado.Add(izquierda[i++]);
+            }
+            else
+            {
+                resultado.Add(derecha[j++]);
+            }
+        }
+        // Agregar los elementos restantes
+        while (i < izquierda.Count)
+        {
+            resultado.Add(izquierda[i++]);
+        }
+        while (j < derecha.Count)
+        {
+            resultado.Add(derecha[j++]);
+        }
+        return resultado;
+    }
+}
+
+//Actividad 3 Busqueda
+
+public class  BuscadorSimplificado
+
+{
+	//Busqueda binaria (lista ordenada por Id)
+	//Hay una tupla que devuelve el producto encontrado y el numero de iteraciones realizadas
+	public static (Producto, int) BusquedaBinaria(List<Producto> productos, int idBuscado)
+    {
+        //Parte de una lista ordenada
+        //Divide en la mitad la lista
+        //1.- Calcular el punto medio
+
+        int inicio = 0;
+        int fin = productos.Count - 1;
+        int iteraciones = 0;
+
+
+        while (inicio <= fin)
+        {
+            iteraciones++;
+
+            //1.- La mitad de la lista ordenada
+            int medio  = inicio + (fin - inicio) / 2;
+
+            //2- Comprobar si encontramos el ID
+
+            if (productos[medio].Id == idBuscado)
+            {
+                return (productos[medio], iteraciones); // Encontrado
+			}
+
+			// 3- Ajustar los limites de busqueda
+            if (productos[medio].Id < idBuscado)
+            {
+                inicio = medio + 1; // Buscar en la mitad derecha
+            }
+            else
+            {
+                fin = medio - 1; // Buscar en la mitad izquierda
+			}
+
+		}
+		return (null, iteraciones); // No encontrado
+    }
+
+    //Busqueda secuencial
+
+    public static (Producto, int) BusquedaSecuencialNombre(List<Producto> productos, string nombreBuscado)
+    {
+        int iteraciones = 0;
+
+        // 1.- Recorrre la lista uno por uno
+
+        foreach (Producto producto in productos)
+        {
+            iteraciones++;
+			//2- Comparamos el nombre (sin distinguir mayusculas/minusculas)
+
+			if (producto.Nombre.Equals(nombreBuscado,StringComparison.OrdinalIgnoreCase))
+            {
+                return (producto, iteraciones); // Encontrado
+            }
+        }
+        return (null, iteraciones); // No encontrado
+	}
+}
+
+
